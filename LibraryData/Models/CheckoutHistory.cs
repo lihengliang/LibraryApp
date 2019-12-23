be1d272735f9e9bc2ac70d674f9b8555d92c8b97
Ethan Liang <liheng.liang@hotmail.com>
2019-12-24 00:48:21 +1100

    Add project files.

---
+++ LibraryData/Models/CheckoutHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryData.Models
+{
+    public class CheckoutHistory
+    {
+        public int Id { get; set; }
+        [Required]
+        public LibraryAsset LibraryAsset { get; set; }
+        public LibraryCard LibraryCard { get; set; }
+        public DateTime CheckedOut { get; set; }
+        public DateTime? CheckedIn { get; set; }
+    }
+}
